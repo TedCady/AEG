1713c8c44d1f421891b26f6c69160e331c58adb4
Chase Smith <chaseasmith95@gmail.com>
2021-04-08 19:05:11 -0500

    Update from 7:01 pm

--- aegApp/urls.py
+++ aegApp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('teams', views.teams),
+    path('contact', views.contact),
+    path('partners', views.partners),
+    path('calendar', views.calendar),
 ]
--- aegApp/views.py
+++ aegApp/views.py
@@ -7,3 +7,23 @@ def home(request):
     # if 'user_id' in request.session:
     #     del request.session['user_id']
     return render(request, "Home.html")
+
+
+def teams(request):
+
+    return render(request, "Teams.html")
+
+
+def partners(request):
+
+    return render(request, "Partners.html")
+
+
+def calendar(request):
+
+    return render(request, "Calendar.html")
+
+
+def contact(request):
+
+    return render(request, "Contact.html")
